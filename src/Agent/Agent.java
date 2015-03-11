874b5c60ce54cd806df9e319ac2ed61e830b8178
magicmikek <koene95@gmail.com>
2015-03-11 12:12:58 +0100

    Added Moving Methods to Agent and SimpleAlgorithm Class

--- src/Agent/Agent.java
+++ src/Agent/Agent.java
@@ -1,16 +1,61 @@
 package Agent;
 
+import java.awt.geom.Point2D;
+
 public class Agent {
 
     private double speed = 1.4;
 	private int maxTurn;
 	private int visionRange;
+    Point2D topLeft;
+    Point2D bottomRight;
+
 	
 	public Agent(){
 		
 	}
-	public void move(){
-		
+	public void move(int direction){
+        // moving up
+        if(direction == 0){
+            double currenty = topLeft.getY();
+            double currentx =  topLeft.getX();
+            currenty--;
+
+            topLeft.setLocation(currentx, currenty);
+        }
+
+        // moving left
+        if(direction == 1){
+
+            double currenty = topLeft.getY();
+            double currentx =  topLeft.getX();
+            currentx--;
+
+            topLeft.setLocation(currentx, currenty);
+
+        }
+
+        // moving right
+        if(direction == 2){
+
+            double currenty = topLeft.getY();
+            double currentx =  topLeft.getX();
+            currentx++;
+
+            topLeft.setLocation(currentx, currenty);
+
+        }
+
+        // moving down
+        if(direction == 3){
+
+            double currenty = topLeft.getY();
+            double currentx =  topLeft.getX();
+            currenty++;
+
+            topLeft.setLocation(currentx, currenty);
+
+        }
 	}
 	public void turn() {
 
@@ -23,5 +68,27 @@ public class Agent {
     public void setSpeed(double speed) {
         this.speed = speed;
     }
+
+    public boolean isInside(Point2D n){
+        return n.getX() > topLeft.getX() && n.getX() < bottomRight.getX() && n.getY() > topLeft.getY() && n.getY() < bottomRight.getY();
+    }
+
+    public Point2D getTopLeft() {
+        return topLeft;
+    }
+
+    public Point2D getBottomRight() {
+        return bottomRight;
+    }
+
+    public void setTopLeft(Point2D point) {
+        topLeft = point;
+    }
+
+    public void setBottomRight(Point2D point) {
+        bottomRight = point;
+    }
+
+
 
 }
---
+++ src/Algorithm/SimpleAlgorithm.java
@@ -0,0 +1,65 @@
+package Algorithm;
+
+
+import Agent.IntruderAgent;
+import Agent.SurveillanceAgent;
+import GameObjects.InanimateObjects;
+import Map.Map;
+
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+
+public class SimpleAlgorithm {
+
+    private SurveillanceAgent sAgent;
+    private IntruderAgent iAgent;
+    private Map map;
+    private ArrayList<InanimateObjects> gameObjects;
+    private ArrayList<SurveillanceAgent> agents;
+
+    public SimpleAlgorithm(Map map){
+        this.map = map;
+        sAgent = new SurveillanceAgent();
+        iAgent = new IntruderAgent();
+        gameObjects = map.getGameObjects();
+
+
+    }
+
+    public void algorithm(){
+
+    }
+
+    public void addAgents(SurveillanceAgent agent){
+            double x = Math.random()*map.getWidth();
+            double y = Math.random()*map.getHeight();
+            Point2D.Double topleft = new Point2D.Double(x, y);
+            double nx = x + (Math.random()*map.getWidth());
+            double ny = y + (Math.random()*map.getHeight());
+            Point2D.Double bottomright = new Point2D.Double(nx, ny);
+            if(emptySpot(topleft) && emptySpot(bottomright)){
+                agent.setTopLeft(topleft);
+                agent.setBottomRight(bottomright);
+                boolean m = false;
+                for(int j = 0;j<gameObjects.size();j++){
+                    if(agent.isInside(gameObjects.get(j).getTopLeft()) == true){
+                        m = true;
+                    }
+                }
+                if(m == false){
+                    agents.add(agent);
+                }
+            }
+    }
+    public boolean emptySpot(Point2D n){
+        boolean answer = true;
+        for(int i=0;i<gameObjects.size();i++){
+            if(gameObjects.get(i).isInside(n)){
+                answer = false;
+            }
+        }
+        return answer;
+    }
+
+
+}
