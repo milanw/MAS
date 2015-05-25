7337c7b16bfae3e74be3454081f1988be94c7b4e
Marco <mar.co@online.de>
2015-05-25 20:12:05 +0200

    combined the 2 additional maps. feel free to come up with a better name

---
+++ src/GUI/DeveloperFrame.java
@@ -0,0 +1,36 @@
+package GUI;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import Agent.InternalMap;
+import Agent.InfluenceMap.InfluenceMap;
+
+public class DeveloperFrame extends JFrame {
+	private InternalMap internalMap; 
+	private InfluenceMap influenceMap; 
+
+	public DeveloperFrame(InternalMap internalMap, InfluenceMap influenceMap) {
+		this.influenceMap = influenceMap; 
+		this.internalMap = internalMap;
+
+		this.setTitle("Internal Map");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		this.setSize(300, 600); 
+		//this.setLocationRelativeTo(null);
+
+		//map view
+		JPanel panel = new JPanel();
+		InternalMapComponent component = new InternalMapComponent(internalMap);
+		InfluenceMapComponent influenceComponent = new InfluenceMapComponent(influenceMap);
+		component.setPreferredSize(new Dimension(300, 300));
+		influenceComponent.setPreferredSize(new Dimension(300, 300));
+		panel.add(component);
+		panel.add(influenceComponent);
+		this.getContentPane().add(panel);
+		this.setVisible(true);
+	}
+}
