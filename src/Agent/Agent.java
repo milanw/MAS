4301bd9864b8bc749fa1c1015ad1ef110fccba70
Marco <mar.co@online.de>
2015-06-09 21:36:57 +0200

    moved some code from agent to surveillanceAgent

--- src/Agent/Agent.java
+++ src/Agent/Agent.java
@@ -17,14 +17,13 @@ import Map.Map;
 import Simulation.Simulator;
 
 public class Agent {
-	private static int idCount = 0; 
+	protected static int idCount = 0; 
 	private static ArrayList<Agent> agents = new ArrayList<Agent>();
 	public static InternalMap internalMap;
 	public static InfluenceMap influenceMap;
-	private boolean explorationFinished = false;
-	public static boolean intruderEntered = false;
 	
-	private int id; 
+	
+	protected int id; 
 	protected Map map; 
     private double speed = 1.4;
     private static int defaultSize = 4; 
@@ -35,9 +34,10 @@ public class Agent {
 	private int viewingAngle; 
     protected Point2D topLeft;
     protected Point2D bottomRight;
+    public static boolean intruderEntered = false;
     private ArrayList<Point2D> path= new ArrayList<Point2D>();
     protected ArrayList<Node> queue = new ArrayList<Node>();
-    private int[] lastPosition = new int[] {-1, -1}; 
+    protected int[] lastPosition = new int[] {-1, -1}; 
 
 	
 	public Agent(Point2D topLeft, Point2D bottomRight, Map map){
@@ -52,31 +52,7 @@ public class Agent {
 		
 	}	
 	
-	//NOT USED ANYMORE!!
-	public Point2D[] pheromoneMove() {
-		Point2D left = new Point2D.Double(topLeft.getX()-getWidth(), topLeft.getY());
-		Point2D right = new Point2D.Double(topLeft.getX()+getWidth(), topLeft.getY());
-		Point2D top = new Point2D.Double(topLeft.getX(), topLeft.getY()-getHeight());
-		Point2D bottom = new Point2D.Double(topLeft.getX(), topLeft.getY()+getHeight());
-		Point2D[] directions = {left, top, bottom, right}; 
-		
-		Point2D result = new Point2D.Double(); 
-		int minMarkers = Integer.MAX_VALUE;
-		
-		for (Point2D d : directions) {
-			Rectangle r = new Rectangle((int)d.getX(), (int)d.getY(), getWidth(), getHeight());
-			Rectangle r2 = new Rectangle((int)d.getX(), (int)d.getY(), getWidth(), getHeight());
-			if (map.checkCollisions(r2)) { 
-				int amountMarkers = map.markersIn(r);
-				if (amountMarkers < minMarkers) {
-					minMarkers = amountMarkers; 
-					result = d; 
-				}
-			}
-		}
-		
-		return new Point2D[] {result, new Point2D.Double(result.getX()+defaultSize, result.getY()+defaultSize)}; 
-	}
+	
 	
 
 	public void getNextMove(double TIME_BETWEEN_UPDATES) {
@@ -201,94 +177,7 @@ public class Agent {
 		return newCurrent;
 	}
 	
-	public void getMove() {
-		if (intruderEntered) {
-			//if (Simulator.intruder.isInVicinity(this))
-			//if (influenceMap.getInfluence(getDiscretePosition()) < 0.8)	{
-			if (!Simulator.intruder.isInVicinity(this)) {
-				greedy();
-			}
-			else
-				return;
-			
-		}
-		else		
-			brickAndMortar();
-		/*if(Simulator.intruder != null && !Simulator.intruder.isInVicinity(this))
-			influenceMap.propagate(new InfluenceNode(getDiscretePosition()[0], getDiscretePosition()[1]), 0.6, -1);*/
-		
-		influenceMap.propagate(new InfluenceNode(getDiscretePosition()[0], getDiscretePosition()[1]), 0.6, -1);
-	}
 	
-	public void greedy() {
-		
-		internalMap.setMap(map.getDiscretizedMap(5));
-		ArrayList<InfluenceNode> adjacent = influenceMap.getNeighbours(new InfluenceNode(getDiscretePosition()[0], getDiscretePosition()[1])); 
-		double[][] influence = influenceMap.getMap();
-		int[][] internal = internalMap.getMap();
-		double bestValue = -2.0; 
-		int[] bestCell = new int[2]; 
-		int[] secondBestCell = new int[2]; 
-		
-		for (InfluenceNode node : adjacent) {
-			
-			if (internal[node.y][node.x] == 1)
-				continue;
-			
-			if (influence[node.y][node.x] > bestValue) {
-				bestValue = influence[node.y][node.x];
-				secondBestCell = bestCell;
-				bestCell = new int[] {node.x, node.y};
-			}
-				
-		}
-		if (lastPosition[0] == bestCell[0] && lastPosition[1] == bestCell[1])
-			moveTo(secondBestCell);
-		else
-			moveTo(bestCell);
-	}
-	public void brickAndMortar() {
-		int[] pos = getDiscretePosition(); 
-		detectWalls(pos);
-		
-		//marking step
-		
-		if (!internalMap.blockingPath(pos)) {
-			//System.out.println("marked as visited: " + pos[0] + " " + pos[1]); 
-			internalMap.setCell(pos, 4); 
-		}
-		else {
-			//System.out.println("marked as explored: " + pos[0] + " " + pos[1]); 
-			internalMap.setCell(pos, 3); 
-		}
-		
-		ArrayList<int[]> explored = internalMap.removeOccupied(internalMap.getCellsAround(pos[0], pos[1], 3));
-		ArrayList<int[]> unexplored = internalMap.removeOccupied(internalMap.getCellsAround(pos[0], pos[1], 0));
-		
-		//navigation step
-		//if at least one of the four cells around is unexplored
-		if (!unexplored.isEmpty()) {
-			//System.out.println("unexplored cells exist"); 
-			int[] p = internalMap.getBestCell(unexplored);
-			//System.out.println(p[0] + " " + p[1] + " " + pos[0] + " " + pos[1]);
-			moveTo(p);
-		}
-		
-		else if (!explored.isEmpty()) {
-			int[] move = explored.remove(id%explored.size());
-			Random rnd = new Random();
-			//int[] move = explored.remove(rnd.nextInt(explored.size()));
-			if (move[0] == lastPosition[0] && move[1] == lastPosition[1] && explored.size()>0)
-				move = explored.get(id%explored.size());
-			moveTo(move);
-		}
-		
-		else {
-			//System.out.println("exploration finished " + unexplored.size());
-			explorationFinished = true; 
-		}
-		lastPosition = pos; 
-	}
 	
 	public void moveTo(int[] pos) {
 		lastPosition = pos;
@@ -456,7 +345,10 @@ public class Agent {
     	return id; 
     }
     
-    
+    //should be overwritten by intruder & surveillance classes
+    public void getMove() {
+    	
+    }
     
     public Point2D.Double getCenter() {
 		double x = (topLeft.getX()+bottomRight.getX())/2;
--- src/Agent/SurveillanceAgent.java
+++ src/Agent/SurveillanceAgent.java
@@ -1,12 +1,18 @@
 package Agent;
 
 import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.Random;
 
+import Agent.InfluenceMap.InfluenceNode;
 import Map.Map;
+import Simulation.Simulator;
 
 public class SurveillanceAgent extends Agent{
     public boolean onSentryTower;
     public int viewingAngle = 45;
+    private boolean explorationFinished = false;
+	
 
 	public SurveillanceAgent(){
 		super();
@@ -34,4 +40,93 @@ public class SurveillanceAgent extends Agent{
             viewingAngle = 45;
         }
     }
+    
+    public void getMove() {
+		if (intruderEntered) {
+			//if (Simulator.intruder.isInVicinity(this))
+			//if (influenceMap.getInfluence(getDiscretePosition()) < 0.8)	{
+			if (!Simulator.intruder.isInVicinity(this)) {
+				greedy();
+			}
+			else
+				return;
+			
+		}
+		else		
+			brickAndMortar();
+		/*if(Simulator.intruder != null && !Simulator.intruder.isInVicinity(this))
+			influenceMap.propagate(new InfluenceNode(getDiscretePosition()[0], getDiscretePosition()[1]), 0.6, -1);*/
+		
+		influenceMap.propagate(new InfluenceNode(getDiscretePosition()[0], getDiscretePosition()[1]), 0.6, -1);
+	}
+	
+	public void greedy() {
+		
+		internalMap.setMap(map.getDiscretizedMap(5));
+		ArrayList<InfluenceNode> adjacent = influenceMap.getNeighbours(new InfluenceNode(getDiscretePosition()[0], getDiscretePosition()[1])); 
+		double[][] influence = influenceMap.getMap();
+		int[][] internal = internalMap.getMap();
+		double bestValue = -2.0; 
+		int[] bestCell = new int[2]; 
+		int[] secondBestCell = new int[2]; 
+		
+		for (InfluenceNode node : adjacent) {
+			
+			if (internal[node.y][node.x] == 1)
+				continue;
+			
+			if (influence[node.y][node.x] > bestValue) {
+				bestValue = influence[node.y][node.x];
+				secondBestCell = bestCell;
+				bestCell = new int[] {node.x, node.y};
+			}
+				
+		}
+		if (lastPosition[0] == bestCell[0] && lastPosition[1] == bestCell[1])
+			moveTo(secondBestCell);
+		else
+			moveTo(bestCell);
+	}
+	public void brickAndMortar() {
+		int[] pos = getDiscretePosition(); 
+		detectWalls(pos);
+		
+		//marking step
+		
+		if (!internalMap.blockingPath(pos)) {
+			//System.out.println("marked as visited: " + pos[0] + " " + pos[1]); 
+			internalMap.setCell(pos, 4); 
+		}
+		else {
+			//System.out.println("marked as explored: " + pos[0] + " " + pos[1]); 
+			internalMap.setCell(pos, 3); 
+		}
+		
+		ArrayList<int[]> explored = internalMap.removeOccupied(internalMap.getCellsAround(pos[0], pos[1], 3));
+		ArrayList<int[]> unexplored = internalMap.removeOccupied(internalMap.getCellsAround(pos[0], pos[1], 0));
+		
+		//navigation step
+		//if at least one of the four cells around is unexplored
+		if (!unexplored.isEmpty()) {
+			//System.out.println("unexplored cells exist"); 
+			int[] p = internalMap.getBestCell(unexplored);
+			//System.out.println(p[0] + " " + p[1] + " " + pos[0] + " " + pos[1]);
+			moveTo(p);
+		}
+		
+		else if (!explored.isEmpty()) {
+			int[] move = explored.remove(id%explored.size());
+			Random rnd = new Random();
+			//int[] move = explored.remove(rnd.nextInt(explored.size()));
+			if (move[0] == lastPosition[0] && move[1] == lastPosition[1] && explored.size()>0)
+				move = explored.get(id%explored.size());
+			moveTo(move);
+		}
+		
+		else {
+			//System.out.println("exploration finished " + unexplored.size());
+			explorationFinished = true; 
+		}
+		lastPosition = pos; 
+	}
 }
