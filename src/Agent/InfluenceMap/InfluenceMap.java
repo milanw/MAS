5c2e0eef2f438fff3150ff875ac8371e8f0d9663
Marco <mar.co@online.de>
2015-06-09 21:56:10 +0200

    2 different decay rates for positive/negative influence

--- src/Agent/InfluenceMap/InfluenceMap.java
+++ src/Agent/InfluenceMap/InfluenceMap.java
@@ -7,22 +7,31 @@ import java.util.ArrayList;
 
 
 public class InfluenceMap {
-	private static double PROPAGATION_CONSTANT = 0.9; 
+	private static double POSITIVE_DECAY = 0.9;
+	private static double NEGATIVE_DECAY = 0.8; 
 	private int width;
 	private int height; 
+	private double[][] positiveMap; 
+	private double[][] negativeMap;
 	private double[][] map; 
+	
 	private int cellWidth = 5;
 
 	public InfluenceMap(int width, int height) {
 		this.width = width/cellWidth;
 		this.height = height/cellWidth; 
+		this.positiveMap = new double[this.height][this.width];
+		this.negativeMap = new double[this.height][this.width];
 		this.map = new double[this.height][this.width];
 	}
 
+	
 	public void decay() {
 		for (int i = 0; i < map.length; i++) {
 			for (int j = 0; j < map[0].length; j++) {
-				map[i][j] *= 0.8;
+				negativeMap[i][j] *= NEGATIVE_DECAY;
+				positiveMap[i][j] *= POSITIVE_DECAY;
+				map[i][j] = positiveMap[i][j] + negativeMap[i][j];
 			}			
 		}
 	}
@@ -31,10 +40,19 @@ public class InfluenceMap {
 		for (int i = 0; i < map.length; i++) {
 			for (int j = 0; j < map[0].length; j++) {
 				int distance =  Math.abs(start.x-j) + Math.abs(start.y-i);
-				//int distance = Math.max(Math.abs(start.x-j), Math.abs(start.y-i)); 
-				map[i][j] += sign * Math.pow(propagationConstant, distance);
-				//map[i][j] += sign *Math.exp(propagationConstant*distance);		
-				map[i][j] = sign < 0 ? Math.max(-1.0, map[i][j]) : Math.min(1.0, map[i][j]);
+				
+				if (sign < 0) {
+					negativeMap[i][j] -= Math.pow(propagationConstant, distance);
+					negativeMap[i][j] = Math.max(-1.0, negativeMap[i][j]);
+				}
+				else {
+					positiveMap[i][j] += Math.pow(propagationConstant, distance);
+					positiveMap[i][j] = Math.min(1.0, positiveMap[i][j]);
+				}
+				
+				map[i][j] = positiveMap[i][j] + negativeMap[i][j];
+					
+				
 			}			
 		}
 	}
@@ -44,9 +62,9 @@ public class InfluenceMap {
 	
 	public ArrayList<InfluenceNode> getNeighbours(InfluenceNode node) {
 		ArrayList<InfluenceNode> neighbours = new ArrayList<InfluenceNode>(); 
-		if (node.x+1 <map[0].length)
+		if (node.x+1 < width)
 			neighbours.add(new InfluenceNode(node.x+1, node.y));
-		if (node.y+1 <map.length)
+		if (node.y+1 < height)
 			neighbours.add(new InfluenceNode(node.x, node.y+1));
 		if (node.x-1 >= 0)
 			neighbours.add(new InfluenceNode(node.x-1, node.y));
@@ -69,12 +87,20 @@ public class InfluenceMap {
 		return map[pos[1]][pos[0]];
 	}
 	
+	public double[][] getPositiveMap() {
+		return positiveMap;
+	}
+	
+	public double[][] getNegativeMap() {
+		return negativeMap;
+	}
+	
 	public static void main(String[] args) {
 		InfluenceMap m = new InfluenceMap(100, 100);
 		m.propagate(new InfluenceNode(5,5), 0.9, 1);
 		//m.propagate2(new Node(10,10));
 		
-		double[][] grid = m.getMap(); 
+		double[][] grid = m.getPositiveMap(); 
 		
 		for (int i = 0; i < grid.length; i++) {
 			for (int j = 0; j < grid[0].length; j++) {
